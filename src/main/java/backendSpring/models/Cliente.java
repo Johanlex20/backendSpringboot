efb935f34dbd5ad22626e6e9af09b180440ba184
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-01-02 15:27:45 -0500

    Clase Entity, Size

--- src/main/java/backendSpring/models/Cliente.java
+++ src/main/java/backendSpring/models/Cliente.java
@@ -3,6 +3,7 @@ package backendSpring.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.Date;
@@ -18,16 +19,21 @@ public class Cliente {
     private Long id;
 
     @NotEmpty
+    @Size(min = 4, max = 15)
     @Column(nullable = false)
     private String nombre;
 
     @NotEmpty
+    @Size(min = 4, max = 15)
     @Column(nullable = false)
     private String apellido;
 
+    @NotEmpty
+    @Size(min = 10,max = 13)
     @Column(nullable = false)
     private String telefono;
 
+    @NotEmpty
     @Column(nullable = false, unique = true)
     @Email
     private String email;
