506ce2407fd27c47b191f5ea1c12f0ba53c6e568
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-01-05 13:13:10 -0500

    agregando app properties link para zona horaria

--- src/main/java/backendSpring/models/Cliente.java
+++ src/main/java/backendSpring/models/Cliente.java
@@ -3,6 +3,7 @@ package backendSpring.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -19,29 +20,28 @@ public class Cliente {
     private Long id;
 
 
-    @NotEmpty(message = "no puede estar vacio")
+    @NotEmpty(message = "Nombre no puede estar vacio")
     @Size(min = 4, max = 12, message = "el tamaño tiene que estar entre 4 y 12 caracteres")
     @Column(nullable = false)
     private String nombre;
 
-    @NotEmpty(message = "no puede estar vacio")
+    @NotEmpty(message = "Apellido no puede estar vacio")
     @Column(nullable = false)
     @Size(min = 4, max = 12, message = "el tamaño tiene que estar entre 4 y 12 caracteres")
     private String apellido;
 
-    @NotEmpty(message = "no puede estar vacio")
+    @NotEmpty(message = "Telefono no puede estar vacio")
     @Size(min = 10, max = 13, message = "el tamaño debe tener 10 numeros")
     @Column(nullable = false)
     private String telefono;
 
-    @NotEmpty(message = "no puede estar vacio")
+    @NotEmpty(message = "Email no puede estar vacio")
     @Email(message = "no es una direccion de correo con un formato valido")
     @Column(nullable = false , unique = true)
     private String email;
 
-
+    @NotNull(message = "Fecha no puede estar vacio")
     @Temporal(TemporalType.DATE)
-    @Column(nullable = false)
     private Date fecha;
 
 }
